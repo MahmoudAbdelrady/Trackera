ae4671e94184d89168ede79554f77ea65d37c7ee
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2025-08-08 08:32:36 +0300

    [Backend] Update email regex in signup dto

--- Backend/src/main/java/com/mdevs/trackera/dto/user/SignUpDTO.java
+++ Backend/src/main/java/com/mdevs/trackera/dto/user/SignUpDTO.java
@@ -18,7 +18,7 @@ public class SignUpDTO {
     private String lastname;
 
     @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}$", message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Password is required")
