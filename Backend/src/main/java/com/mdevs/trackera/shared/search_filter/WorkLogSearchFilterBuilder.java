73ea342e7c10d57f21256cb919ce002a7c9aecba
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2025-09-11 08:30:35 +0300

    [Backend] Support evaluation search

--- Backend/src/main/java/com/mdevs/trackera/shared/search_filter/WorkLogSearchFilterBuilder.java
+++ Backend/src/main/java/com/mdevs/trackera/shared/search_filter/WorkLogSearchFilterBuilder.java
@@ -4,6 +4,7 @@ import com.mdevs.trackera.entity.WorkLog;
 import com.mdevs.trackera.shared.exceptions.types.BusinessException;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Predicate;
+import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -15,11 +16,12 @@ import java.util.Set;
 public class WorkLogSearchFilterBuilder {
     private final List<SearchFilter> filters = new ArrayList<>();
 
-    private final static Set<String> ALLOWED_FIELDS = Set.of("name", "totalHours", "status", "dateFrom", "dateTo");
+    private final static Set<String> ALLOWED_FIELDS = Set.of("name", "totalHours", "status", "dateFrom", "dateTo", "evaluation");
 
     public WorkLogSearchFilterBuilder(List<SearchFilter> searchFilters) {
         if (searchFilters != null && !searchFilters.isEmpty()) {
             validateDateFilters(searchFilters);
+            List<SearchFilter> processedFilters = new ArrayList<>();
             for (SearchFilter filter : searchFilters) {
                 validateSearchFilter(filter);
 
@@ -27,8 +29,28 @@ public class WorkLogSearchFilterBuilder {
                     filter.setFieldName("workDate");
                 }
 
-                addFilter(filter);
+                processedFilters.addAll(handleFilter(filter));
             }
+            processedFilters.forEach(this::addFilter);
+        }
+    }
+
+    private void validateSearchFilter(SearchFilter searchFilter) {
+        if (StringUtils.isEmpty(searchFilter.getFieldName())) {
+            throw new BusinessException("Field name is required in search filter");
+        }
+        if (!ALLOWED_FIELDS.contains(searchFilter.getFieldName())) {
+            throw new BusinessException("Filtering by field '" + searchFilter.getFieldName() + "' is not supported");
+        }
+        if (searchFilter.getValue() == null || StringUtils.isEmpty(searchFilter.getValue().toString())) {
+            throw new BusinessException("Value is required for field: " + searchFilter.getFieldName());
+        }
+
+        if (searchFilter.getOperator() == null) {
+            searchFilter.setOperator(getDefaultSearchOperator(searchFilter.getFieldName()));
+        }
+        if (searchFilter.getOperator().equals(SearchOperator.BETWEEN) && (searchFilter.getExtraValue() == null || StringUtils.isEmpty(searchFilter.getExtraValue().toString()))) {
+            throw new BusinessException("Extra value is required for BETWEEN operator on field: " + searchFilter.getFieldName());
         }
     }
 
@@ -53,23 +75,46 @@ public class WorkLogSearchFilterBuilder {
         }
     }
 
-    private void validateSearchFilter(SearchFilter searchFilter) {
-        if (StringUtils.isEmpty(searchFilter.getFieldName())) {
-            throw new BusinessException("Field name is required in search filter");
+    private List<SearchFilter> handleFilter(SearchFilter filter) {
+        if (filter.getFieldName().equals("evaluation")) {
+            return validateEvaluationFilter(filter);
+        } else {
+            return List.of(filter);
         }
-        if (!ALLOWED_FIELDS.contains(searchFilter.getFieldName())) {
-            throw new BusinessException("Filtering by field '" + searchFilter.getFieldName() + "' is not supported");
-        }
-        if (searchFilter.getValue() == null || StringUtils.isEmpty(searchFilter.getValue().toString())) {
-            throw new BusinessException("Value is required for field: " + searchFilter.getFieldName());
-        }
+    }
 
-        if (searchFilter.getOperator() == null) {
-            searchFilter.setOperator(getDefaultSearchOperator(searchFilter.getFieldName()));
+    private List<SearchFilter> validateEvaluationFilter(SearchFilter filter) {
+        if (!EnumUtils.isValidEnum(WorkLog.Evaluation.class, filter.getValue().toString())) {
+            throw new BusinessException("Invalid evaluation type: " + filter.getValue());
         }
-        if (searchFilter.getOperator().equals(SearchOperator.BETWEEN) && (searchFilter.getExtraValue() == null || StringUtils.isEmpty(searchFilter.getExtraValue().toString()))) {
-            throw new BusinessException("Extra value is required for BETWEEN operator on field: " + searchFilter.getFieldName());
+        if (this.filters.stream().anyMatch(f -> f.getFieldName().equals("totalHours"))) {
+            throw new BusinessException("Cannot filter by both evaluation and total hours");
         }
+        WorkLog.Evaluation evaluationFilter = WorkLog.Evaluation.valueOf(filter.getValue().toString());
+        filter.setFieldName("totalHours");
+        switch (evaluationFilter) {
+            case EXCELLENT -> {
+                filter.setOperator(SearchOperator.GREATER_THAN_EQUAL);
+                filter.setValue("8");
+                return List.of(filter);
+            }
+            case GOOD -> {
+                SearchFilter greaterThanFilter = new SearchFilter("totalHours", SearchOperator.GREATER_THAN_EQUAL, "7.5");
+                SearchFilter lessThanFilter = new SearchFilter("totalHours", SearchOperator.LESS_THAN, "8");
+                return List.of(greaterThanFilter, lessThanFilter);
+            }
+            case MODERATE -> {
+                SearchFilter greaterThanFilter = new SearchFilter("totalHours", SearchOperator.GREATER_THAN_EQUAL, "7");
+                SearchFilter lessThanFilter = new SearchFilter("totalHours", SearchOperator.LESS_THAN, "7.5");
+                return List.of(greaterThanFilter, lessThanFilter);
+            }
+            case POOR -> {
+                filter.setOperator(SearchOperator.LESS_THAN);
+                filter.setValue("7");
+                return List.of(filter);
+            }
+        }
+        return List.of();
     }
 
     public void addFilter(SearchFilter filter) {
