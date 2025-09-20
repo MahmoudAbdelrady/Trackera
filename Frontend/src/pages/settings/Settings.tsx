d647169e4ae4460d222e89ca73e13202a74b7502
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2025-09-20 18:04:19 +0300

    [Frontend] Integration with backend for linking jira account

--- Frontend/src/pages/settings/Settings.tsx
+++ Frontend/src/pages/settings/Settings.tsx
@@ -4,11 +4,15 @@ import { SettingsSection } from "../../components";
 import classes from "./scss/settings.module.css";
 
 const Settings = () => {
+  const linkJiraAccount = () => {
+    window.location.href = `${import.meta.env.VITE_TRACKERA_BACKEND_URL}/auth/oauth-v2/jira`;
+  };
+
   return (
     <AppLayout>
       <div className={classes.settings_sections}>
         <SettingsSection title="Linked Accounts" icon={<Link />}>
-          <LinkedAccount platform="Jira" icon={<img src="/Assets/jira_icon.svg" alt="Jira" />} accountIdentifier="user@example.com" isLinked={true} onLink={() => {}} onUnlink={() => {}} />
+          <LinkedAccount platform="Jira" icon={<img src="/Assets/jira_icon.svg" alt="Jira" />} accountIdentifier="user@example.com" isLinked={false} onLink={linkJiraAccount} onUnlink={() => {}} />
         </SettingsSection>
       </div>
     </AppLayout>
