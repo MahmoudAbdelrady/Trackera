0a54477fbe722065cfbd910da61a8954994e6776
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2026-01-09 13:17:06 +0200

    fix: update resolveClientKey method user retrieval imp

--- Backend/src/main/java/com/mdevs/trackera/aspect/RateLimitAspect.java
+++ Backend/src/main/java/com/mdevs/trackera/aspect/RateLimitAspect.java
@@ -1,5 +1,7 @@
 package com.mdevs.trackera.aspect;
 
+import com.mdevs.trackera.config.general.AppConfig;
+import com.mdevs.trackera.entity.User;
 import com.mdevs.trackera.shared.rate_limit.ApiRateLimiter;
 import com.mdevs.trackera.shared.annotations.RateLimited;
 import com.mdevs.trackera.shared.exceptions.types.RateLimitExceededException;
@@ -8,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -32,8 +35,9 @@ public class RateLimitAspect {
     }
 
     private String resolveClientKey() {
-        if (request.getUserPrincipal() != null) {
-            return request.getUserPrincipal().getName();
+        User currentUser = AppConfig.getCurrentUser();
+        if (currentUser != null) {
+            return String.join("_", currentUser.getId().toString(), currentUser.getFirstname());
         }
         return request.getRemoteAddr();
     }
